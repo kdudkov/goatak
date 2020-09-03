cdd0c2458e2fc80a31071da2f544aa7933708d36
Konstantin Dudkov <kdudkov@ya.ru>
2020-09-03 22:03:07 +0300

    fix

--- static/js/main.js
+++ static/js/main.js
@@ -164,6 +164,9 @@ let app = new Vue({
             let d = new Date(Date.parse(str));
             return ("0" + d.getDate()).slice(-2) + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" +
                 d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+        },
+        sp: function (v) {
+            return (v * 3.6).toFixed(1);
         }
     },
 });
