94a7ac17a1b5c6c4090f05c08f2b87f42c2e2c39
Konstantin Dudkov <kdudkov@ya.ru>
2022-04-04 23:22:07 +0300

    fix test

--- model/icons_test.go
+++ model/icons_test.go
@@ -5,10 +5,10 @@ import (
 )
 
 func TestSiDC(t *testing.T) {
-	checkSIDC(t, "a-u-G", "SUG-------")
-	checkSIDC(t, "a-f-G-U-C", "SFG-UC----")
-	checkSIDC(t, "a-n-A-C-F", "SNA-CF----")
-	checkSIDC(t, "a-f-G-wasp-struct", "SFG-------")
+	checkSIDC(t, "a-u-G", "SUGP------")
+	checkSIDC(t, "a-f-G-U-C", "SFGPUC----")
+	checkSIDC(t, "a-n-A-C-F", "SNAPCF----")
+	checkSIDC(t, "a-f-G-wasp-struct", "SFGP------")
 }
 
 func checkSIDC(t *testing.T, fn, sidc string) {
