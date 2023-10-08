92f3b7170e7b2aaff61c322d223748c4ea6893fb
Konstantin Dudkov <kdudkov@ya.ru>
2023-10-08 18:46:31 +0300

    fix userctl

--- internal/model/user.go
+++ internal/model/user.go
@@ -2,10 +2,10 @@ package model
 
 type UserInfo struct {
 	User     string `yaml:"user"`
-	Callsign string `yaml:"callsign"`
-	Team     string `yaml:"team"`
-	Role     string `yaml:"role"`
-	Typ      string `yaml:"type"`
+	Callsign string `yaml:"callsign,omitempty"`
+	Team     string `yaml:"team,omitempty"`
+	Role     string `yaml:"role,omitempty"`
+	Typ      string `yaml:"type,omitempty"`
 	Password string `yaml:"password"`
-	Scope    string `yaml:"scope"`
+	Scope    string `yaml:"scope,omitempty"`
 }
