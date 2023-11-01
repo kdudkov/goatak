48346f727b138f9cd3dcb4649a7c08e9dab025a5
Konstantin Dudkov <kdudkov@ya.ru>
2023-11-02 02:09:42 +0300

    docker

--- docker_build.sh
+++ docker_build.sh
@@ -2,4 +2,11 @@
 
 ver=$(git describe --always --dirty)
 docker build . -t kdudkov/goatak_server:$ver -t kdudkov/goatak_server:latest
-docker push kdudkov/goatak_server
+
+if [[ $ver == v* ]]; then
+  echo "pushing $ver"
+  docker push kdudkov/goatak_server:$ver
+fi
+
+echo "pushing latest"
+docker push kdudkov/goatak_server:latest
