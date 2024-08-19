40938beec77bdcae0679ee94a8334f4052204863
Konstantin Dudkov <kdudkov@ya.ru>
2024-08-20 01:48:47 +0300

    feature: air -> fiber

--- pkg/log/fiber_logger.go
+++ pkg/log/fiber_logger.go
@@ -3,9 +3,29 @@ package log
 import (
 	"fmt"
 	"log/slog"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	httpRequestsDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "goatak",
+		Subsystem: "http",
+		Name:      "request_duration_seconds",
+		Help:      "The latency of the HTTP requests.",
+		Buckets:   prometheus.DefBuckets,
+	}, []string{"api"})
+
+	httpRequestsCount = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "goatak",
+		Subsystem: "http",
+		Name:      "requests_total",
+		Help:      "Number of the HTTP requests.",
+	}, []string{"api", "path", "method", "code"})
 )
 
 func NewFiberLogger(api string, username func(c *fiber.Ctx) string) fiber.Handler {
@@ -15,6 +35,8 @@ func NewFiberLogger(api string, username func(c *fiber.Ctx) string) fiber.Handle
 		start := time.Now()
 		chainErr := c.Next()
 		wt := time.Since(start)
+
+		metrics(api, c, wt)
 
 		msg := fmt.Sprintf("%d %s %s %s", c.Response().StatusCode(), c.Method(), c.Path(), c.Request().URI().QueryArgs().String())
 		l := logger
@@ -52,4 +74,15 @@ func NewFiberLogger(api string, username func(c *fiber.Ctx) string) fiber.Handle
 
 		return nil
 	}
+}
+
+func metrics(api string, ctx *fiber.Ctx, t time.Duration) {
+	httpRequestsDuration.With(prometheus.Labels{"api": api}).Observe(t.Seconds())
+
+	httpRequestsCount.With(prometheus.Labels{
+		"api":    api,
+		"path":   ctx.Path(),
+		"method": ctx.Method(),
+		"code":   strconv.Itoa(ctx.Response().StatusCode()),
+	}).Inc()
 }
