aca4e0b04e15b51824f4e866719aaeee7144c142
Konstantin Dudkov <kdudkov@ya.ru>
2024-08-20 11:10:25 +0300

    docker build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+image="ghcr.io/kdudkov/goatak_server"
+#image="kdudkov/goatak_server"
 ver=$(git describe --always --tags --dirty)
 branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
-docker build . --build-arg branch=$branch --build-arg commit=$ver -t kdudkov/goatak_server:$ver -t kdudkov/goatak_server:latest
+docker build . --build-arg branch=$branch --build-arg commit=${ver} -t ${image}:${ver} -t ${image}:latest
 
-echo "$ver"
+echo "${ver}"
 
-if [[ $ver != *-* ]]; then
-  echo "pushing $ver"
-  docker push kdudkov/goatak_server:$ver
+if [[ ${ver} != *-* ]]; then
+  echo "pushing ${ver}"
+  docker push ${image}:${ver}
 fi
 
 echo "pushing latest"
-docker push kdudkov/goatak_server:latest
+docker push ${image}:latest
