38fd54b4b4ff51eb8c7253534946f7eabcb245d3
Konstantin Dudkov <kdudkov@ya.ru>
2025-06-15 20:21:39 +0300

    new prefs

--- cmd/goatak_server/mp/prefs_test.go
+++ cmd/goatak_server/mp/prefs_test.go
@@ -4,11 +4,25 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/kdudkov/goatak/pkg/util"
 )
 
 func TestGetPref(t *testing.T) {
-	for _,s := range []string{"locationTeam", "hidePreferenceItem_routePreference"} {
+	for _, s := range []string{"locationTeam", "hidePreferenceItem_routePreference"} {
 		e := GetEntry(s, "aaa")
 		assert.NotEmpty(t, e)
+	}
+}
+
+func TestDouble(t *testing.T) {
+	s := util.NewStringSet()
+
+	for _, p := range prefKeys {
+		if s.Has(p.Key) {
+			t.Errorf(" dublicate key %s", p.Key)
+		}
+
+		s.Add(p.Key)
 	}
 }
